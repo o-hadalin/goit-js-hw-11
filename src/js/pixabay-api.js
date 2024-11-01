0dbb80e9be70f6c29a64852c0c64fadfd829acc2
O. HADALIN <dyblikat777@gmail.com>
2024-11-01 21:47:20 +0200

    Pixabay-api start

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+const fetchImages = (query) => {
+  const API_KEY = "46849808-268f228f7185f073a2d253586";
+  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+  return fetch(URL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);  
+      }
+      return response.json();
+    })
+    .then(data => data.hits)
+    .catch(error => {
+      console.error("Error fetching data:", error);
+      return [];
+    });
+};
