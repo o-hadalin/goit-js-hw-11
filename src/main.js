64c7eacc8e9b7037d91209758a3e9c008d41c4ce
O. HADALIN <dyblikat777@gmail.com>
2024-11-02 22:19:39 +0200

    Library's install + main

---
+++ src/main.js
@@ -0,0 +1,42 @@
+import { fetchImages } from "./pixabay-api";
+import { renderGallery } from "./render-functions";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector("form");
+const gallery = document.querySelector(".gallery");
+const loader = document.querySelector(".loader");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const query = event.target.elements.query.value.trim();
+
+  if (!query) {
+    iziToast.warning({ message: "Please enter a search term." });
+    return;
+  }
+
+  loader.classList.add("show");
+  gallery.innerHTML = "";
+
+  fetchImages(query)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((images) => {
+      loader.classList.remove("show");
+      if (images.length > 0) {
+        renderGallery(images, gallery);
+      } else {
+        iziToast.error({ message: "Sorry, there are no images matching your search query. Please try again!" });
+      }
+    })
+    .catch((error) => {
+      loader.classList.remove("show");
+      console.error("Error fetching images:", error);
+      iziToast.error({ message: "An error occurred while fetching images. Please try again later." });
+    });
+});
