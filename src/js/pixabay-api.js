b591af3414477716f3c954d131fdfb43ce7cbfce
O. HADALIN <dyblikat777@gmail.com>
2024-11-04 01:23:31 +0200

    URLSearchParams

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,15 @@
 export const fetchImages = (query) => {
   const API_KEY = "46849808-268f228f7185f073a2d253586";
-  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+ const params = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
 
+  const URL = `https://pixabay.com/api/?${params.toString()}`;
+  
   return fetch(URL)
       .then(response => {
         console.log(response);
