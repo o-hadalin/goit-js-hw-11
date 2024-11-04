fbd0d45c2d15a850c6db364fc66337a5101e6a17
O. HADALIN <dyblikat777@gmail.com>
2024-11-04 02:20:24 +0200

    Gallery's CSS

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -5,9 +5,30 @@ export function renderGallery(images, galleryElement) {
   galleryElement.innerHTML = images.map(img => `
     <a href="${img.largeImageURL}">
       <img src="${img.webformatURL}" alt="${img.tags}">
-      <div>👍 ${img.likes} 👀 ${img.views} 💬 ${img.comments} ⬇️ ${img.downloads}</div>
+      <div class="card-info">
+        <div class="info-row">
+          <span class="label">Likes</span>
+          <span class="value">${img.likes}</span>
+        </div>
+        <div class="info-row">
+          <span class="label">Views</span>
+          <span class="value">${img.views}</span>
+        </div>
+        <div class="info-row">
+          <span class="label">Comments</span>
+          <span class="value">${img.comments}</span>
+        </div>
+        <div class="info-row">
+          <span class="label">Downloads</span>
+          <span class="value">${img.downloads}</span>
+        </div>
+      </div>
     </a>
   `).join("");
-  const lightbox = new SimpleLightbox("a", { /* опції */ });
+  
+  const lightbox = new SimpleLightbox("a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
   lightbox.refresh();
 }
